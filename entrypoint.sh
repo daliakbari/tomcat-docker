5e552fc7bc1d072951a0ebc0001fcee1c1050aa8
Dawod <1758638+daliakbari@users.noreply.github.com>
2021-09-27 16:55:57 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,24 +7,24 @@ set -e
 
 if [ "$1" = 'start-tomcat.sh' ] || [ "$1" = 'catalina.sh' ]; then
 
-    USER_ID=${TOMCAT_USER_ID:-1000}
-    GROUP_ID=${TOMCAT_GROUP_ID:-1000}
+    USER_ID=${TOMCAT_USER_ID:-1000770000}
+    GROUP_ID=${TOMCAT_GROUP_ID:-1000770000}
 
     ###
     # Tomcat user
     ###
-    groupadd -r tomcat -g ${GROUP_ID} && \
-    useradd -u ${USER_ID} -g tomcat -d ${CATALINA_HOME} -s /sbin/nologin \
-        -c "Tomcat user" tomcat
+    groupadd -r modirsan -g ${GROUP_ID} && \
+    useradd -u ${USER_ID} -g modirsan -d ${CATALINA_HOME} -s /sbin/nologin \
+        -c "Tomcat user" modirsan
 
     ###
     # Change CATALINA_HOME ownership to tomcat user and tomcat group
     # Restrict permissions on conf
     ###
 
-    chown -R tomcat:tomcat ${CATALINA_HOME} && chmod 400 ${CATALINA_HOME}/conf/*
+    chown -R modirsan:modirsan ${CATALINA_HOME} && chmod 400 ${CATALINA_HOME}/conf/*
     sync
-    exec gosu tomcat "$@"
+    exec gosu modirsan "$@"
 fi
 
 exec "$@"
